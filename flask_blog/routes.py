62fa1ef6ce5f9e51aa7659175cce58c46fa50f94
BegisM <ugolokbegis@gmail.com>
2023-12-06 18:21:59 +0100

    pagination and posts features

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', default=1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template('home.html', posts=posts)
 
 
@@ -82,16 +83,15 @@ def save_picture(form_picture):
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        with app.app_context():
-            current_user.username = form.username.data
-            current_user.email = form.email.data
+        current_user.username = form.username.data
+        current_user.email = form.email.data
 
-            if form.picture.data:
-                picture_file = save_picture(form.picture.data)
-                current_user.image_file = picture_file
-            
-            db.session.add(current_user)
-            db.session.commit()
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        
+        db.session.add(current_user)
+        db.session.commit()
             
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
@@ -102,8 +102,6 @@ def account():
     print(image_file)
     return render_template('account.html', title='account',
                            image_file=image_file, form=form)
-
-
 
 
 @app.route("/post/new", methods=['GET', 'POST'])
@@ -119,7 +117,6 @@ def new_post():
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', 
                            form=form, legend='New Post')
-
 
 
 @app.route("/post/<int:post_id>")
@@ -138,10 +135,10 @@ def update_post(post_id):
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
-        with app.app_context():    
-            post.title = form.title.data
-            post.content = form.content.data
-            db.session.commit()
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.add(post)
+        db.session.commit()
         flash('Your post has been updated!', 'success')
         return redirect(url_for('post', post_id=post.id))
     elif request.method == 'GET':
@@ -150,3 +147,28 @@ def update_post(post_id):
         
     return render_template('create_post.html', title='Update Post', 
                            form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', default=1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(per_page=5, page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
